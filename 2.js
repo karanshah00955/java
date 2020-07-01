402bfa9ed0c4b02685d4f88ea2e9fd05699503fd
karanshah00955 <karan.shah00777@gmail.com>
2020-07-01 16:16:02 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+console.log("hello yaakov")
+var names = ["john ", " jen ", "jason", "jim"];
+for(var name in names){
+  console.log(" goodbye" + names[name]);
+}
+
+var names2=["Paul","Frank","Larry","Paula","Laura"];
+ for(var i in names2){
+ 	console.log("hello " + names2[i]);
+ }
+
